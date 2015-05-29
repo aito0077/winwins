cab24aa8b2202fa517a57de72bd3ff1fec2a4652
apivetta <aguspivetta@gmail.com>
2015-05-29 17:20:32 -0300

    scaffolding del sitio

---
+++ public_dev/app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name winwinsApp
+ * @description
+ * # winwinsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('winwinsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+
+    $stateProvider.state('main',{
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      }).state('about',{
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      });
+  });
---
+++ public_dev/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name winwinsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the winwinsApp
+ */
+angular.module('winwinsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
