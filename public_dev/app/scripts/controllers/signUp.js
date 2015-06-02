518acb7791b297d76130ba8c84f204ef92a2aceb
apivetta <aguspivetta@gmail.com>
2015-06-02 17:46:26 -0300

    validaciones form

--- public_dev/app/scripts/controllers/signUp.js
+++ public_dev/app/scripts/controllers/signUp.js
@@ -9,9 +9,7 @@
  */
 angular.module('winwinsApp')
   .controller('SignUpCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.signupForm = function(){
+    	console.log("submit");
+    }
   });
