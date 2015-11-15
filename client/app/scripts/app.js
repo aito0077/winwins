efae89700436ac885e7e20e0c797bd232465f935
Linkway <manuel@linkway.me>
2015-11-15 19:15:36 -0300

    'merge'

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -379,7 +379,7 @@ angular.module('winwinsApp', [
         'background-attachment': attrs.backgroundAttachment,
         'background-origin': attrs.backgroundOrigin,
         'background-clip': attrs.backgroundClip,
-        'background': '  linear-gradient(rgb(0, 0, 0), rgba(154, 148, 120, 0.0980392)),url(' + attrs.backgroundImage + ')',
+        'background': 'linear-gradient(rgba(0, 0, 0, 0.73), rgba(154, 148, 120, 0.0980392)),url(' + attrs.backgroundImage + ')',
         'background-size': 'cover',
        });
       };
