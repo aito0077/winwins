085a327e643476ee727e3defad68c75eb1ef1bef
José Luis Di Biase <josx@interorganic.com.ar>
2016-03-21 15:49:37 -0300

    new frontend app: using generator-gulp-angular
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

---
+++ frontend/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('frontend', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
