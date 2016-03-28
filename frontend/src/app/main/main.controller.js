2490c610c757f452024b48eebf430c51c2fd0061
tiko2015 <tiko2015@gmail.com>
2016-03-28 16:24:53 -0300

    agregado del video en home

--- frontend/src/app/main/main.controller.js
+++ frontend/src/app/main/main.controller.js
@@ -6,13 +6,12 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr, sponsors, winwins, miembros, partners) {
+  function MainController($timeout, sponsors, winwins, miembros, partners) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1458584706984;
-    vm.showToastr = showToastr;
     vm.sponsors = sponsors;
     vm.winwins = winwins;
     vm.miembros = miembros;
@@ -75,26 +74,5 @@
       }
     ];
 
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
