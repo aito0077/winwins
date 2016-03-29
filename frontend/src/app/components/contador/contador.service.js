e3d8f53ad29486a97ca784612e7544633ee1a97a
Alina Ryba <alinaryba@gmail.com>
2016-03-29 16:10:36 -0300

    Modulo contador en home

---
+++ frontend/src/app/components/contador/contador.service.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  angular
+      .module('winwins')
+      .service('contador', contador);
+
+  /** @ngInject */
+  function contador($log, Restangular, $q) {
+
+    var _partner = {};
+
+    var testData = [
+      { id: 1, nombre: 'Partner1' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet1.com' },
+      { id: 2, nombre: 'Partner2' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet2.com' },
+      { id: 3, nombre: 'Partner3' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet3.com' },
+      { id: 4, nombre: 'Partner4' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet4.com' },
+      { id: 5, nombre: 'Partner5' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet5.com' },
+      { id: 6, nombre: 'Partner6' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet6.com' },
+      { id: 7, nombre: 'Partner7' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet7.com' },
+      { id: 8, nombre: 'Partner8' , imagen: 'assets/images/partner.jpg', link: 'http://www.partnet8.com' }
+    ];
+
+    _contador.getList = function() {
+      var dfd = $q.defer();
+      dfd.resolve(testData);
+      return dfd.promise;
+    };
+
+    return _contador;
+  }
+
+})();
