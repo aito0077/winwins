bffd4c191c7558b496e738a63028a4b2c79a4bcb
Adrian Pardini <adrian@camba.coop>
2016-03-29 12:40:28 -0300

    panel: namespace para controllers
    (cherry picked from commit c7a033b919183f58688cbe4f7622a36a66d049a3)

--- backend/config/panel.php
+++ backend/config/panel.php
@@ -1,21 +1,18 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 return array(
-    
+    'controllers' => 'Winwins\Http\Controllers\Admin',
     'crudItems' => array(
-       
+
     ),
     'panelControllers' => array(
-       'Admin', 'Link'  
+       'Admin', 'Link',
     ),
     'logo'=>'packages/serverfireteam/panel/img/logo.png' // logo of Panel 
-    
-);
-    
-    
 
+);
