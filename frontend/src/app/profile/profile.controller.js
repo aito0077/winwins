d0d7acb0352a94d5eb18cd3560b29e94839b7891
nancel <nestor.ancel@gmail.com>
2016-04-12 15:58:27 -0300

    se cargan los datos del usuario logueado, y se pueden guardar cambios, menos las imágenes

--- frontend/src/app/profile/profile.controller.js
+++ frontend/src/app/profile/profile.controller.js
@@ -6,10 +6,23 @@
     .controller('ProfileController', ProfileController);
 
   /** @ngInject */
-  function ProfileController() {
+  function ProfileController(account, user) {
     var vm = this;
 
+    account.getProfile()
+    .then(function(data) {
+      user.getUser(data.user.id)
+      .then(function(user_data) {
+        vm.user = user_data;
+        if (vm.user.birthdate) {
+          vm.user.birthdate = new Date(vm.user.birthdate);
+        }
+      });
+    });
 
+    vm.saveProfile = function() {
+      user.saveProfile(vm.user);
+    };
   }
 
 })();
