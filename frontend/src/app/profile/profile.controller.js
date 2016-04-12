4f8e64f7383d4a44e6f9d50d40a5738b2de330e8
nancel <nestor.ancel@gmail.com>
2016-04-12 18:33:55 -0300

    se agregan popup para cropear imagenes

--- frontend/src/app/profile/profile.controller.js
+++ frontend/src/app/profile/profile.controller.js
@@ -6,7 +6,7 @@
     .controller('ProfileController', ProfileController);
 
   /** @ngInject */
-  function ProfileController(account, user) {
+  function ProfileController(account, user, $mdDialog) {
     var vm = this;
 
     account.getProfile()
@@ -23,6 +23,74 @@
     vm.saveProfile = function() {
       user.saveProfile(vm.user);
     };
+
+    vm.showCropAvatarDialog = function(ev) {
+      $mdDialog.show({
+        controller: CropAvatarController,
+        templateUrl: 'app/profile/avatar_crop.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true
+      })
+      .then(function(image) {
+        vm.avatar_image = image;
+
+      });
+    };
+
+    vm.showCropCoverDialog = function(ev) {
+      $mdDialog.show({
+        controller: CropCoverController,
+        templateUrl: 'app/profile/cover_crop.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true
+      })
+      .then(function(image) {
+        vm.cover_image = image;
+
+      });
+    };
+  }
+
+  function CropAvatarController($scope, $mdDialog) {
+    $scope.myImage='';
+    $scope.myCroppedImage='';
+
+    $scope.handleFileSelect = function(evt) {
+      var file=evt.files[0];
+      var reader = new FileReader();
+      reader.onload = function (evt) {
+        $scope.$apply(function($scope){
+          $scope.myImage=evt.target.result;
+        });
+      };
+      reader.readAsDataURL(file);
+    };
+
+    $scope.cropImage = function() {
+      $mdDialog.hide($scope.myCroppedImage);
+    }
+  }
+
+  function CropCoverController($scope, $mdDialog) {
+    $scope.myImage='';
+    $scope.myCroppedImage='';
+
+    $scope.handleFileSelect = function(evt) {
+      var file=evt.files[0];
+      var reader = new FileReader();
+      reader.onload = function (evt) {
+        $scope.$apply(function($scope){
+          $scope.myImage=evt.target.result;
+        });
+      };
+      reader.readAsDataURL(file);
+    };
+
+    $scope.cropImage = function() {
+      $mdDialog.hide($scope.myCroppedImage);
+    }
   }
 
 })();
